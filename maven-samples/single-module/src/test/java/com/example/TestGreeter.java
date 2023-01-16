92abbed6b4c23d806f9c0fb9731679fbebc941b0
lin <unknopu@gmail.com>
2023-01-16 22:18:08 +0700

    test jenkins log

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me Happy najaa";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
